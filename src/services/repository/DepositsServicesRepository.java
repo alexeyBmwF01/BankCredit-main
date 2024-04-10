9f6e9e7fceaee0d346920b49d3f2ab80421b0919
Alexey Laktionov <lepexa@yandex.ru>
2024-04-10 19:29:09 +0300

    add method getListBestDepositsDTO

--- src/services/repository/DepositsServicesRepository.java
+++ src/services/repository/DepositsServicesRepository.java
@@ -1,4 +1,10 @@
 package services.repository;
 
+import DTO.DepositsDTO;
+
+import java.util.List;
+
 public interface DepositsServicesRepository {
+    List<DepositsDTO> getListDepositsDTO();
+    List<DepositsDTO> getBestListDepositsDTO();
 }
